bd83cccc7b0bc333e7a6c1df06468b8573c2b38f
Justice-Tei21 <jusnwa050826@student.jenseneducation.se>
2022-02-17 14:54:20 +0100

    tacka mig om det behövs

--- TextBasedGame/main.py
+++ TextBasedGame/main.py
@@ -20,7 +20,7 @@ running = True
 
 
 
-#sheet.append(["ID",'Room','Room Description', 'Paths'])
+
 
 print(str(library.cell(1,1).value))
 
@@ -31,13 +31,18 @@ mapx=0
 mapy=0
 
 def goThroughSheet(thesheet):
+    main_map=[]
     row_number=0
     column_number=0
     for row in sheet:
         row_number+=1
+        column_rooms=[]
         for column in row:
+            column_rooms.append(thesheet.cell(row_number,column_number).value)
             column_number+=1
             print(thesheet.cell(row_number,column_number).value)
+        main_map.append(column_rooms)
+
 
 def look(room_value):
      

5875b1111c737d4036239024f8825ca7773ed8d9
Justice-Tei21 <jusnwa050826@student.jenseneducation.se>
2022-03-10 13:25:58 +0100

    Nu fungerar move

--- TextBasedGame/main.py
+++ TextBasedGame/main.py
@@ -12,12 +12,6 @@ GameMap = workbook['Karta']
 
 
 
-x=3
-y=2
-room_description= 3
-
-running = True
-
 
 
 #sheet.append(["ID",'Room','Room Description', 'Paths'])
@@ -31,15 +25,16 @@ mapx=0
 mapy=0
 
 def goThroughSheet(thesheet):
-    main_map=[]
-    row_number=0
-    column_number=0
+    # goes through a sheet and puts every value into a 2d matrix (I think i can use that word)
+    main_map=[] # the matrix that is going to be returned
+    #row_number=0
+    #column_number=0
     for row in thesheet:
-        row_number+=1
+        #row_number+=1
         column_rooms=[]
         for column in row:
-            column_number+=1
-            column_rooms.append(str(column.value))
+            #column_number+=1
+            column_rooms.append(str(column.value))#adds a cell from sheet row to the lists row
         main_map.append(column_rooms)
     return main_map
 
@@ -50,54 +45,68 @@ def look(room_value):
 def save(direction):# här kommer spar funktionen
     pass
 
-def move(direction):#- här kommer rörelse funktionen
-    global x
-    global y
+def move(x,y,direction):#- här kommer rörelse funktionen
+
     direction.lower()
     if direction =='north':
-        y=+1
+        y-=1
     elif direction =='south':
-        y=-1
+        y+=1
     elif direction =='east':
-        x=+1
+        x+=1
     elif direction =='west':
-        x=-1
+        x-=1
     else:
         print('Enter a valid direction')
+    return x , y
 
 
-for row in goThroughSheet(GameMap):
-    print(row)
+#print(goThroughSheet(GameMap))
 
+def main():
+    x = 0
+    y = 1
+    room_description = 3
 
-
-while running:
-
-    main_map= goThroughSheet(GameMap)
+    running = True
+    room_IDs = goThroughSheet(sheet)
+    main_map = goThroughSheet(GameMap)
     player_position = main_map[y][x]
-
-    a = input()
-    if a == 'look':
-       look()
-
-
-    if a == 'save':
-        save()
-
-    if re.search('move ',a):
-        text = a
-        text = re.sub('move ', '', text)
-
-        print (text)
-        move(text)
-
-    if a == 'quit':
-       running= False
+    while running:
 
 
 
+        a = input()
+        if a == 'look':
+           look()
 
 
+        if a == 'save':
+            save()
+
+        if re.search('move ',a):
+            text = a
+            text = re.sub('move ', '', text)
+
+
+
+            x,y = move(x,y,text)
+            print(text)
+        if a == 'quit':
+           running= False
+
+
+        player_position = main_map[y][x]
+
+        print(player_position)
+
+        print(str(x) + ' ' + str(y))
+
+
+
+
+
+main()
 #sheet.append(["1",'hallway','you see a brightly lit hallway infront of you', 'North,South'])
 
 #workbook.save("hello_world.xlsx")
